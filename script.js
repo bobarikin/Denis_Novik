9798d4c55df322fd8778eee72f17c8053d33b993
Artyom <a.s.bobarikin@gmail.com>
2022-10-16 16:24:21 +0300

    close animate

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const burger = document.querySelector('.burger')
 const popUp = document.querySelector('.popup')
-const close = document.querySelector('.close')
+const burger = document.querySelector('.burger')
+const burger_top = document.querySelector('.burger_top')
+const burger_bot = document.querySelector('.burger_bot')
+const close = document.querySelectorAll('.close')
 
 burger.addEventListener('click', () => {
-  popUp.style.display = 'block'
-})
-
-close.addEventListener('click', () => {
-  popUp.style.display = 'none'
+  console.log(close);
+  close[0].classList.toggle('close_right')
+  close[1].classList.toggle('close_left')
+  popUp.classList.toggle('popUp-active')
+  burger_top.classList.toggle('hidden')
+  burger_bot.classList.toggle('hidden')
+  console.dir(close);
+  if (popUp.classList.contains('popUp-active')) {
+    document.body.style.overflow = 'hidden'
+  } else {
+    document.body.style.overflow = ''
+  }
 })
